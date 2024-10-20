912c6c2c0d8d0d17d8f278f589d4ad9ed3ef0e4f
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-10-20 16:06:18 +0600

    Improve dashboard design with updated icons and layout enhancements
    - Added new icons for Booking and Signout actions
    - Refined button styles with hover effects for better UX
    - Improved overall alignment and spacing for a cleaner look
    - Applied dark mode support for dashboard elements

--- prosaanti/prosaanti/settings.py
+++ prosaanti/prosaanti/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'bootstrap5',
     'accounts',
     'home',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
--- prosaanti/prosaanti/urls.py
+++ prosaanti/prosaanti/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('super/admin/', admin.site.urls),
+    path('admin/', include('dashboard.urls')),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
 ]
