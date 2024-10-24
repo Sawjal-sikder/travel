665f0f7ff0d6271e7eb17413a15ac6ca6bc7d479
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-10-24 16:22:05 +0600

    clint count display

--- prosaanti/accounts/context_processors.py
+++ prosaanti/accounts/context_processors.py
@@ -7,7 +7,12 @@ def userprofile(request):
             profile = userProfile.objects.get(user=request.user)
         except userProfile.DoesNotExist:
             profile = None
+        try:
+            clint_count = userProfile.objects.filter(is_new=True).count()
+        except userProfile.DoesNotExist:
+            clint_count = 22
     else:
         profile = None
+        clint_count = 0
 
-    return {'profile': profile}
+    return {'profile': profile, 'clint_count': clint_count}
