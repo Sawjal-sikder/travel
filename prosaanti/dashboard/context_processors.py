1408fccffcc151d2ab646c08434477657c9b9c85
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-11-07 13:29:14 +0600

    not auth

--- prosaanti/dashboard/context_processors.py
+++ prosaanti/dashboard/context_processors.py
@@ -1,12 +1,10 @@
 from .models import CompanyInformation
 
+
 def com_info(request):
-    if request.user.is_authenticated:
-        try:
-            info = CompanyInformation.objects.get(pk=1)
-        except CompanyInformation.DoesNotExist:
-            info = None
-    else:
+    try:
+        info = CompanyInformation.objects.get(pk=1)
+    except CompanyInformation.DoesNotExist:
         info = None
 
     return {'info': info}
